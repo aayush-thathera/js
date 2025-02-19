916360b87705ccddf78d9b33e39f0afe8cb8800f
aayush-thathera <aayushthathera6gmail.com>
2025-02-19 23:34:10 +0530

    update

---
+++ lower_uppercase.js
@@ -0,0 +1,8 @@
+let text = "Hello, World!";
+let upperCaseText = text.toUpperCase();
+let lowerCaseText = text.toLowerCase();
+
+console.log("Uppercase:", upperCaseText);
+console.log("Lowercase:", lowerCaseText);
+// and try alert
+alert("this is lower case and uper case web page");
